07e214eb67e92290db57960c3b3fcaa298b47d5a
Nick Rolfe <nickrolfe@gmail.com>
2023-04-29 18:18:50 +0100

    D3D11: add DrawIndexed

--- libs/zwin32/src/d3d11.zig
+++ libs/zwin32/src/d3d11.zig
@@ -675,6 +675,20 @@ pub const IDeviceContext = extern struct {
                 @ptrCast(*const IDeviceContext.VTable, self.__v)
                     .Draw(@ptrCast(*IDeviceContext, self), VertexCount, StartVertexLocation);
             }
+            pub inline fn DrawIndexed(
+                self: *T,
+                IndexCount: UINT,
+                StartIndexLocation: UINT,
+                BaseVertexLocation: INT,
+            ) void {
+                @ptrCast(*const IDeviceContext.VTable, self.__v)
+                    .DrawIndexed(
+                    @ptrCast(*IDeviceContext, self),
+                    IndexCount,
+                    StartIndexLocation,
+                    BaseVertexLocation,
+                );
+            }
             pub inline fn Map(
                 self: *T,
                 pResource: *IResource,
@@ -856,7 +870,7 @@ pub const IDeviceContext = extern struct {
             ?[*]const *IClassInstance,
             UINT,
         ) callconv(WINAPI) void,
-        DrawIndexed: *anyopaque,
+        DrawIndexed: *const fn (*T, UINT, UINT, INT) callconv(WINAPI) void,
         Draw: *const fn (*T, UINT, UINT) callconv(WINAPI) void,
         Map: *const fn (
             *T,
